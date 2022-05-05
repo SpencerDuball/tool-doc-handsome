fb52b0fb48b4b14a72b7787ca7b39271a0f75810
Spencer Duball <spencer@spencerduball.com>
2022-05-04 19:32:09 -0500

    Going to NextJs (1) Would prefer to handle IaC myself, not sure if a persistent SQL server is the right move (2) Without the backend, redwood seems to be overkill (3) Will rely on contentlayer and it has support for NextJs currently

--- web/src/components/FavoriteButton/FavoriteButton.stories.tsx
+++
@@ -1,7 +0,0 @@
-import FavoriteButton from './FavoriteButton'
-
-export const generated = () => {
-  return <FavoriteButton aria-label="Favorite this" />
-}
-
-export default { title: 'Components/FavoriteButton' }
--- web/src/components/ProductGrid/ProductGrid.stories.tsx
+++
@@ -1,7 +0,0 @@
-import ProductGrid from './ProductGrid'
-
-export const generated = () => {
-  return <ProductGrid />
-}
-
-export default { title: 'Components/ProductGrid' }
