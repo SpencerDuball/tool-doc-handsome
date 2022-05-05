3c03be384599354f599fb9932afc76630b3bcdb1
Spencer Duball <spencer@spencerduball.com>
2022-05-04 21:16:16 -0500

    Setup chakra-ui on the main project (not storybook or jest yet)

---
+++ frontend/pages/_document.tsx
@@ -0,0 +1,15 @@
+import { ColorModeScript } from "@chakra-ui/react";
+import NextDocument, { Html, Head, Main, NextScript } from "next/document";
+
+export default class Document extends NextDocument {
+  render() {
+    return (
+      <Html lang="en">
+        <Head />
+        <ColorModeScript initialColorMode="system" />
+        <Main />
+        <NextScript />
+      </Html>
+    );
+  }
+}
